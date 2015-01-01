5519a9db7f9e05154f7b3f57191b58ffc228e380
Kieron Wilkinson <kieron@softpres.org>
2015-01-01 21:25:18 +0000

    Provide cell indexing for upcoming Ant algo

--- src/org/softpres/langtonsant/Controller.java
+++ src/org/softpres/langtonsant/Controller.java
@@ -11,43 +11,23 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toList;
 
 public class Controller implements Initializable {
 
-  private static final int RANGE = 100;
-  private static final int CELL_SIZE = 4;
   private static final int FPS = 30;
 
-  public Group grid;
+  public Group cells;
+  private Grid grid;
   private Timeline timeline = new Timeline();
 
   @Override
   public void initialize(URL url, ResourceBundle resourceBundle) {
-    // I'm forcing myself to use a stream here, but without for combinations or a zip method
-    // it's not very nice, and the imperative for loop would probably have been nicer
-    Stream<Rectangle> cells = IntStream.range(0, RANGE).boxed().flatMap(
-          x -> IntStream.range(0, RANGE).mapToObj(y -> cell(x, y)));
-
-    grid.getChildren().addAll(cells.collect(toList()));
-  }
-
-  private Rectangle cell(int x, int y) {
-    Rectangle cell = new Rectangle();
-    cell.setWidth(CELL_SIZE);
-    cell.setHeight(CELL_SIZE);
-    cell.setX(x + (x * CELL_SIZE));
-    cell.setY(y + (y * CELL_SIZE));
-    return cell;
+    grid = new Grid(cells);
   }
 
   public void start(ActionEvent actionEvent) {
@@ -60,7 +40,7 @@ public class Controller implements Initializable {
   private void tick(ActionEvent event) {
     // todo Currently just Random
     Random r = new Random();
-    grid.getChildren().stream().forEach(node -> ((Rectangle)node).setFill(Color.gray(r.nextDouble())));
+    grid.cells().forEach(node -> node.setFill(Color.gray(r.nextDouble())));
   }
 
 }
---
+++ src/org/softpres/langtonsant/Grid.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2015, Kieron Wilkinson
+ */
+package org.softpres.langtonsant;
+
+import javafx.scene.Group;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * Represents the cells on a grid.
+ */
+public class Grid {
+
+  private static final int RANGE = 100;
+  private Cell[][] grid = new Cell[RANGE][RANGE];
+
+  public Grid(Group group) {
+    List<Cell> cells = createCells().collect(toList());
+    cells.forEach(cell -> grid[cell.x()][cell.y()] = cell);
+    group.getChildren().addAll(cells);
+  }
+
+  private Stream<Cell> createCells() {
+    // I'm forcing myself to use a stream here, but without for combinations or a zip method
+    // it's not very nice, and the imperative for loop would probably have been nicer
+    return IntStream.range(0, RANGE).boxed().flatMap(
+          x -> IntStream.range(0, RANGE).mapToObj(y -> new Cell(x, y)));
+  }
+
+  public Stream<Cell> cells() {
+    return Stream.of(grid).flatMap(Arrays::stream);
+  }
+
+}
