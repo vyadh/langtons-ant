84f14202ad753987c7445c0ef6a03ff70be8c390
Kieron Wilkinson <kieron@softpres.org>
2015-01-22 21:46:38 +0000

    Retro implementation

--- src/main/java/org/softpres/langtonsant/Transforms.java
+++ src/main/java/org/softpres/langtonsant/Transforms.java
@@ -3,12 +3,11 @@
  */
 package org.softpres.langtonsant;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
@@ -27,13 +26,11 @@ public class Transforms {
    * This is used to create the number of simulated ants specified by the user.
    */
   public List<Ant> createAnts(int count, Supplier<Ant> factory) {
-    return Stream.generate(factory).limit(count).collect(Collectors.toList());
-
-//    List<Ant> ants = new ArrayList<>(count);
-//    for (int i = 0; i < count; i++) {
-//      ants.add(factory.get());
-//    }
-//    return ants;
+    List<Ant> ants = new ArrayList<>(count);
+    for (int i = 0; i < count; i++) {
+      ants.add(factory.get());
+    }
+    return ants;
   }
 
   /**
@@ -41,18 +38,13 @@ public class Transforms {
    * requires an x and y coordinate, but the stream can return cell in any order.
    */
   public Stream<Cell> createCells(int dimension) {
-    // I'm forcing myself to use a stream here, but without for combinations or a zip method
-    // it's not very nice, and the imperative for loop would probably have been nicer
-    return IntStream.range(0, dimension).boxed().flatMap(
-          x -> IntStream.range(0, dimension).mapToObj(y -> new Cell(x, y)));
-
-//    List<Cell> cells = new ArrayList<>(dimension * dimension);
-//    for (int x = 0; x < dimension; x++) {
-//      for (int y = 0; y < dimension; y++) {
-//        cells.add(new Cell(x, y));
-//      }
-//    }
-//    return cells.stream();
+    List<Cell> cells = new ArrayList<>(dimension * dimension);
+    for (int x = 0; x < dimension; x++) {
+      for (int y = 0; y < dimension; y++) {
+        cells.add(new Cell(x, y));
+      }
+    }
+    return cells.stream();
   }
 
   /**
@@ -63,23 +55,11 @@ public class Transforms {
    * cell in state number 0.
    */
   public LifeCycle createLifeCycle(String tape) {
-    // Scala:
-    // new LifeCycle(tape.map(_ match {
-    //   case 'L' => (d: Direction) => d.turnLeft
-    //   case 'R' => (d: Direction) => d.turnRight
-    // })
-
-    return new LifeCycle(tape.chars()
-          .mapToObj(c -> (char)c)
-          .map(this::turn)
-          .collect(Collectors.toList())
-    );
-
-//    List<Function<Direction, Direction>> result = new ArrayList<>();
-//    for (char c : tape.toCharArray()) {
-//      result.add(turn(c));
-//    }
-//    new LifeCycle(result);
+    List<Function<Direction, Direction>> result = new ArrayList<>();
+    for (char c : tape.toCharArray()) {
+      result.add(turn(c));
+    }
+    return new LifeCycle(result);
   }
 
   private Function<Direction, Direction> turn(char c) {
@@ -96,13 +76,11 @@ public class Transforms {
    * this is just when the reset button is pressed to reset cell state.
    */
   public Stream<Cell> cells(Cell[][] grid) {
-    return Stream.of(grid).flatMap(Arrays::stream);
-
-//    List<Cell> result = new ArrayList<>(grid.length * grid.length);
-//    for (Cell[] row : grid) {
-//      Collections.addAll(result, row);
-//    }
-//    return result.stream();
+    List<Cell> result = new ArrayList<>(grid.length * grid.length);
+    for (Cell[] row : grid) {
+      Collections.addAll(result, row);
+    }
+    return result.stream();
   }
 
 }
